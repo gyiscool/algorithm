7c4c6e798b0591eb4a82bbe471c41ee7e072e88c
gengyang <997485446@qq.com>
2018-10-12 17:35:23 +0800

    Signed-off-by: gengyang <997485446@qq.com>

--- 2.cpp
+++ 2.cpp
@@ -6,53 +6,248 @@ typedef struct PolyNode *Polynomial;
 
 //定义节点结构 
 struct PolyNode{
-	int coef;
-	int expon;
+	int coef;//系数 
+	int expon;//指数 
 	Polynomial link; 
 };
 
 //返回头部指针 
 Polynomial CreateQueue(){
-	Polynomial head;
+	PolyNode *head;
 	head = (Polynomial) malloc(sizeof(struct PolyNode));
 	
-	head->coef = 1;
-	head->expon = 1;
+	head->coef = 0;
+	head->expon = 0;
+	head->link = NULL;
+	
+	return head;
+} 
+
+void printNode(PolyNode *head){
+
+	do{
+		//是否是零多项式 
+		if(head->coef == 0 && head->expon ==0 && head->link == NULL  ){
+			printf("0 0");
+			break;
+		}
+		if(head->coef != 0 || head->expon !=0){
+			printf("%d %d",head->coef,head->expon);
+			if(head->link != NULL) printf(" ");
+		}
+		head = head->link;
+	}while(head != NULL);	
+
+} 
+
+//链表追加结构体元素 系数从大到小 自动找顺序 合并相同指数项的 系数 
+PolyNode *push(PolyNode *head,int coef,int expon){
+	Polynomial now,pre = head ,p = head;
+	
+	if (coef ==0 && expon == 0){
+		return head;
+	} 
+	//获取最后一个元素的指针 把系数大的放后面 系数从大到小  pre  
+	while(   pre != NULL &&(pre->expon > expon || (pre->expon == 0 && pre->coef == 0))){
+		p = pre;
+		pre = pre->link; 
+	}
+
+	//有系数相同的情况 计算结果 
+	if(pre != NULL && (pre->expon == expon)){
+		 pre->coef = pre->coef+coef; 
+		 
+		 //清除 
+		 if(pre->coef == 0){
+		 	p->link = pre->link; 
+		 
+		 }
+		 
+		 return head;
+	}
+		
+	//把类型的地址空间交过来 
+	now = ( PolyNode *) malloc(sizeof(struct PolyNode));
+	now->coef = coef;
+	now->expon = expon;
+	now->link = NULL;
+	
+	if(p->link != NULL){
+		now->link = p->link;
+	}
+	
+	p->link = now ;
+
+	return head;
+}
+
+Polynomial CopyNodes(PolyNode *copy){
+	PolyNode *head;
+	head = CreateQueue();
+	
+	copy = copy->link;
+	
+	while(copy != NULL){
+		push(head,copy->coef,copy->expon);
+		copy = copy->link;
+	}
+	
+	return head;
+} 
+
+//追加 
+PolyNode *pushNodes(PolyNode *head,PolyNode *next){
+	
+	//获取最后一个元素的指针 把系数大的放后面 系数从大到小  pre  
+	while(next != NULL){
+		push(head,next->coef,next->expon);
+		next= next->link;
+		
+	}
+	
+
 	return head;
 } 
 
 
-Polynomial add(Polynomial head){
-	Polynomial next;
-	
-	//把类型的地址空间交过来 
-	next = (Polynomial) malloc(sizeof(struct PolyNode));
-	next->coef = 2;
-	next->expon = 2;
-	head->link = next ;
 
-	return head;
+//两个链表相加
+PolyNode *Add(PolyNode *head1,PolyNode *head2) {
+
+	//p1,p2是两个哨兵，newHead 新返回的头部 
+	PolyNode *p1,*p2,*newHead;
+	p1 = head1;
+	p2 = head2; 
+	newHead = CreateQueue();
+	int coef,expon;
+	
+	
+	//循环对比  
+	while(p2 != NULL && p1 !=NULL) {
+		
+		//如果指数相等 
+		if(p1->expon == p2->expon){
+			expon = p1->expon; 
+			coef = p1->coef+p2->coef;
+			
+			//移动指针 
+			p2 = p2->link;
+			p1 = p1->link;
+			
+			if(coef == 0 ) continue;
+			
+		}else if(p1->expon > p2->expon){ //第一个指数大 
+			coef = p1->coef;
+			expon = p1->expon;
+			
+			//移动指针
+			p1 = p1->link;
+			
+		}else{//第二个指数大 
+			coef = p2->coef;
+			expon = p2->expon;
+			
+			//移动指针
+			p2 = p2->link;
+			
+		}
+		 
+		 //为 新链表 追加元素
+		push(newHead,coef,expon);
+		
+	}
+	
+	
+	if(p2 == NULL || p1 != NULL){
+		pushNodes(newHead,p1);
+	}
+	
+	
+	if(p1 == NULL || p2 != NULL){
+		pushNodes(newHead,p2);
+	}
+		
+	
+	return newHead;
+	
+}
+
+//读取数据 
+PolyNode *ReadPoly(){
+	PolyNode *p1;
+	p1  =CreateQueue();
+	int N,c,e;
+	
+	scanf("%d",&N);
+	
+	while(N--){
+		scanf("%d %d",&c,&e);
+		push(p1,c,e);
+	}
+	
+	return  p1;
+} 
+
+//乘法 
+PolyNode *Mult(PolyNode *head1,PolyNode *head2){
+	PolyNode *newNodes,*temp,*head3,*temp1;
+	
+	newNodes = CreateQueue();
+	
+	temp   = CopyNodes(head2);
+	temp1 = temp;
+	
+	
+	while(head1 != NULL ){
+		head3 = head2;
+		temp = temp1;
+		
+		if(head1->coef == 0) {
+			head1 = head1->link;
+			continue;
+		};
+		
+		while(head3 != NULL){
+
+			temp->coef = (head3->coef*head1->coef);
+			temp->expon = (head3->expon+head1->expon);
+			if(head3->coef == 0 && head3->expon == 0) {
+				temp->coef = 0;
+				temp->expon = 0;	
+			}
+		
+			head3 = head3->link;
+			temp = temp->link;	
+		}
+
+	   
+		
+		newNodes =Add(newNodes,temp1);
+		
+		
+		head1 = head1->link;
+		
+	} 
+	
+	
+	return  newNodes;
 } 
 
 int main(){
 	
-	Polynomial P1,P2,PP,PS;
-	
+	Polynomial P1,P2,PS,PM;
+	//printf("请输入：")	;
 	P1 = ReadPoly();
 	P2 = ReadPoly();
-	PP = Mult(P1,P2); //
-	PrintPoly(PP);
-	PS = Add(P1,P2)
-	
-	PrintPoly(PS);
-	
-	
-	Polynomial q= CreateQueue();
-	add(q);
-	
-	printf("The char is %d -- %#x\n",q->link->coef); //否则打印这个字符和它的键值
-	
+
+
+	PS = Add(P1,P2); 
+	PM = Mult(P1,P2);
+
+	printNode(PM);
+	printf("\n");
+	printNode(PS);
+	 
+
 	return 0;
 }
-
-
--- MaxSubseqSum.cpp
+++ MaxSubseqSum.cpp
@@ -1,162 +1,121 @@
 #include <stdio.h>
+#include<stdlib.h>
 #define countof( array ) (sizeof( array )/sizeof( array[0] ))
-int MaxSubseqSumq(int A[],int N);
-int MaxSubseqSum1(int A[],int start,int end);
-int MaxSubseqSumq2(int A[],int N); 
+
+
+
+struct Node{
+	int sumNum; 
+	int firstNum; 
+	int endNum;
+}  ;
+
+Node *MaxSubseqSumq2(int A[],int N); 
+
 int main()
 {
-	char c;
-	int d[5] = {22,-20,11,90,80};
-
-
-	printf("当前序列:");
-	int j = countof(d);
-
-	for(int i = 0;i<j;i++){
-		printf("%d、",d[i]);
-	} 
+	int c;
 	
+	int N ,j ;
 	
-	//排序功能
-	//int k = MaxSubseqSumq(d,j);
-	//int k =MaxSubseqSum1(d,0,j-1);
-	int k = MaxSubseqSumq2(d,j);
+	scanf("%d",&N);
 	
-	printf("The char is %d -- %#x\n",k); //否则打印这个字符和它的键值
-	 
-	//接受输入 
-	/*for (;;) //无限循环
-	{
-		c=getchar(); // 读一字符
-		//scanf("%d%s",d,c);
-		if (c=='\n'){
-			break; //若是 新行键 跳出 循环
-		}else{
-			printf("The char is %d -- %#x\n",c); //否则打印这个字符和它的键值
-			d = (int)c;
-		}
+	j = N;
+	
+	int d[N];
+	
+	while(N-- > 0){
+		scanf("%d",&c);
+		d[j-N-1] = c;
 	}
-	*/
+	
+	Node *k;
+
+	k = MaxSubseqSumq2(d,j);
+	
+	printf("%d ",k->sumNum);
+	printf("%d ",k->firstNum);
+	printf("%d",k->endNum);
 	
 	return 0;
 }
 
 //在线查找
-int MaxSubseqSumq2(int A[],int N){
+Node *MaxSubseqSumq2(int A[],int N){
 	
+	Node *node;
 	int i;
+	int endNumbyIndex;
 	int maxNum = 0; 
-	int thisNum = 0; 
-	
+	int thisNum = 0;
+	 
+	node = ( Node * ) malloc(sizeof(struct Node));
+	node->sumNum = 0;
+	node->firstNum = 0;
+	node->endNum = 0;
+
 	//循环计算每一个当前数字 到最后一数字连续的子列值 
 	for( i=0;i < N; i++){
+		
 		if(thisNum + A[i] <0 ) {
-			thisNum = 0;
+			thisNum = 0; 
 			continue;
 		}
-	
 		
+		//没有缩小 
 		thisNum += A[i];
 		
-		if(maxNum < thisNum) maxNum=thisNum; 
+		//更新 
+		if(maxNum < thisNum) {
+			endNumbyIndex = i;
+			maxNum=thisNum;
+		}
+		 
 
+	}
+	
+	node->sumNum = maxNum;
+	node->endNum = A[endNumbyIndex];
+	
+	if(maxNum <=0) {
+		int b = 0;
+		for( i=0;i < N; i++){
+		
+			if(A[i] >=0 )  b =1;
+		}
+	
+		node->firstNum = A[0];
+		node->endNum = A[N-1];
+		printf("");
+		if(b != 1){
+
+			return node;
+		}else{
+			node->firstNum = 0;
+			node->endNum = 0 ;
+			return node;
+		}
+		
 		
 	}
 	
-	return maxNum;
+	for(int j =0;j<=endNumbyIndex; j++){
+		maxNum =  maxNum-A[endNumbyIndex-j];
+		
+		if(maxNum <= 0){
+			node->firstNum = A[endNumbyIndex-j];
+			break;
+	}
+		
+
+	
+	} 
+	
+
+	
+	
+	return node;
 
 } 
  
-
-//二分法
-int MaxSubseqSum1(int A[],int start,int end){
-	
-	int midNum = (start+end)/2; 
-	int maxSum =0;
-	int thisSum = 0;
-	
-	//输出当前数字 
-	if((end-start)<=0)
-		return A[start];
-		
-	//中间参数 
-	
-	//左边一半 右边一半 
-	int leftSum =  MaxSubseqSum1(A,start,midNum); 
-	int rightSum =  MaxSubseqSum1(A,midNum+1,end); 
-	
-	maxSum = leftSum; 
-	if(leftSum < rightSum) maxSum = rightSum; 
-	 
-	
-	//循环遍历 获取跨越边界最大值
-//	thisSum = A[midNum];
-	for(int i=0;i<(end-midNum+1);i++){
-		
-		if(midNum-i >=start ) thisSum += A[midNum-i];
-		
-		if(thisSum > maxSum) maxSum=thisSum;
-		
-		if( (midNum+1+i) <=end ) thisSum += A[midNum+1+i];
-		
-		if(thisSum > maxSum) maxSum=thisSum;	
-
-	}	
-
-	return maxSum;
-	
-} 
-
-
-//莎莎循环 
-int MaxSubseqSumq(int A[],int N){
-	
-	int i,j,k;
-	int maxNum = 0; 
-	int thisNum = 0; 
-	
-	//循环计算每一个当前数字 到最后一数字连续的子列值 
-	for( i=0;i < N; i++){
-		thisNum = 0;// 每一次循环重新设置零 当前和
-		
-		//循环子列值 
-		for( j=i;j < N; j++){
-		//	printf("%d、",A[j]);
-			thisNum +=A[j]; 
-			if(thisNum > maxNum) 
-				maxNum = thisNum;
-			
-		}
-		
-	}
-	
-	return maxNum;
-
-} 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
