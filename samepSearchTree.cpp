eaf85e8a4c88abed8fba62e95852f0f0625cc10b
GengY <997485446@qq.com>
2018-10-17 22:08:10 +0800

    Signed-off-by: GengY <997485446@qq.com>

---
+++ samepSearchTree.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+
+void readInput(){
+	int size,lineNum;
+	scanf("%d %d ",&size,&lineNum);
+	
+	//读取第一组构成二叉树 
+	
+	
+	
+}
+
+int main(){
+	
+	scanf();
+	
+	return 0;
+} 
